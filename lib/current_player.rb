f7d1223784a316593a1edbce6917012193b05bc5
Souloinc87 <souloinc87@gmail.com>
2018-02-19 11:59:14 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   counter = 0
-  board.each do |index| 
+  board.each do |index|
 
     if index = "X" || index = "O"
       counter += 1
       puts "#{counter}"
-    end 
+    end
   end
 end
